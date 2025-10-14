c0c20a2899f15ffdf7cb0e1e7f51a93caa91217f
carlos-sweb <c4rl0sill3sc4@gmail.com>
2025-10-14 01:58:27 -0300

    add uv scraping

---
+++ move_data.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash 
+cp "scrapy/uv/data/uv.json" "www/data/uv.json"
---
+++ scrapy/uv/scrapy.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash 
+qjs -e \
+"$(\
+	curl -s \
+	https://archivos.meteochile.gob.cl/portaldmc/meteochile/js/indice_radiacion.js \
+	| sed -e 's/&Aacute;/Á/g'\
+	-e 's/&Eacute;/É/g'\
+	-e 's/&Iacute;/Í/g'\
+	-e 's/&Oacute;/Ó/g'\
+	-e 's/&Uacute;/Ú/g'\
+	-e 's/&aacute;/á/g'\
+	-e 's/&eacute;/é/g'\
+	-e 's/&iacute;/í/g'\
+	-e 's/&oacute;/ó/g'\
+	-e 's/&uacute;/ú/g'\
+	-e 's/&ntilde;/ñ/g'\
+	-e 's/&Ntilde;/Ñ/g'\
+	-e 's/&amp;/\&/g'\
+	-e 's/&#039;/\'"'"'/g'\
+
+);\
+print(JSON.stringify(RadiacionUVB,null,2));" | \
+jq '[.[] | select(.enservicio == true) | { indice_num:(.indiceobs | split(":")[0] | tonumber ),indice_str:(.indiceobs | split(":")[1] ),name:.nombre}]' \
+> data/uv.json
